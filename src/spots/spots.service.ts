0baa8c510363e0ca83ae66ac4dc3e9e3ad2aa552
Igor Gabriel <rogiborgessouza@outlook.com>
2024-06-18 16:43:48 -0300

    feat: create spots routes

--- src/spots/spots.service.ts
+++ src/spots/spots.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSpotDto } from './dto/create-spot.dto';
 import { UpdateSpotDto } from './dto/update-spot.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SpotStatus } from '@prisma/client';
 
+export type CreateSpotInput = CreateSpotDto & { eventId: string };
 @Injectable()
 export class SpotsService {
-  create(createSpotDto: CreateSpotDto) {
-    return 'This action adds a new spot';
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createSpotDto: CreateSpotInput) {
+    const event = await this.prismaService.event.findFirst({
+      where: {
+        id: createSpotDto.eventId,
+      },
+    });
+
+    if (!event) {
+      throw new Error('Event not found');
+    }
+
+    return this.prismaService.spots.create({
+      data: {
+        ...createSpotDto,
+        status: SpotStatus.avaliable,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all spots`;
+  findAll(eventId: string) {
+    return this.prismaService.spots.findMany({
+      where: {
+        eventId,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} spot`;
+  findOne(eventId: string, spotId: string) {
+    return this.prismaService.spots.findFirst({
+      where: {
+        id: spotId,
+        eventId,
+      },
+    });
   }
 
-  update(id: number, updateSpotDto: UpdateSpotDto) {
-    return `This action updates a #${id} spot`;
+  update(eventId: string, spotId: string, updateSpotDto: UpdateSpotDto) {
+    return this.prismaService.spots.update({
+      data: updateSpotDto,
+      where: {
+        id: spotId,
+        eventId,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} spot`;
+  remove(eventId: string, spotId: string) {
+    return this.prismaService.spots.delete({
+      where: {
+        id: spotId,
+        eventId,
+      },
+    });
   }
 }
